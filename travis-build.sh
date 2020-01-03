b7713429f50f934dbd59c5476b6210e6ce75f531
Björn Schrader <BjoernSch@users.noreply.github.com>
2020-01-04 00:46:03 +0100

    Update travis-build.sh

--- travis-build.sh
+++ travis-build.sh
@@ -6,14 +6,14 @@ set -xe
 if [[ -z "$DOCKERFILE" ]]; then
 	docker build --build-arg BUKKIT_VERSION=$TAG -t bjoernsch/bukkit:$TAG .
 else
-	docker build -f $DOCKERFILE --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
+	docker build -f $DOCKERFILE --build-arg BUKKIT_VERSION=$TAG -t bjoernsch/bukkit:$TAG .
 fi
 
 # Test
 docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bjoernsch/bukkit:$TAG
 
 if [[ "$TRAVIS_BRANCH" = "master" ]]; then
-	docker push bbriggs/bukkit:$TAG
+	docker push bjoernsch/bukkit:$TAG
 else
 	exit 0
 fi
