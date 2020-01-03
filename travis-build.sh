86a3f4b31f1d2c23a211a790089f1c1a50774971
Björn Schrader <BjoernSch@users.noreply.github.com>
2020-01-04 00:40:55 +0100

    Change docker links

--- travis-build.sh
+++ travis-build.sh
@@ -4,13 +4,13 @@ set -xe
 
 # Build
 if [[ -z "$DOCKERFILE" ]]; then
-	docker build --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
+	docker build --build-arg BUKKIT_VERSION=$TAG -t bjoernsch/bukkit:$TAG .
 else
 	docker build -f $DOCKERFILE --build-arg BUKKIT_VERSION=$TAG -t bbriggs/bukkit:$TAG .
 fi
 
 # Test
-docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bbriggs/bukkit:$TAG
+docker run -it -p 25565:25565 -v /data:/data -e EULA=true -e TRAVIS=true bjoernsch/bukkit:$TAG
 
 if [[ "$TRAVIS_BRANCH" = "master" ]]; then
 	docker push bbriggs/bukkit:$TAG
